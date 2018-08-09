090823c5402d64b033f1f5b89ee43174e82c1195
cfdahill <cfdahill@gmail.com>
2018-08-09 14:49:50 -0700

    organized structure

---
+++ client/src/api/youtubeVideos.js
@@ -0,0 +1,12 @@
+//most of this should be accessible directly from youtube which would make life easier
+
+{
+  videos: [{
+    title: "asdf",
+    url: "asdf",
+    tags: ["assdf", "asdf", "etc"],
+    published: "01/01/2018",
+    description: "asdf"
+  }
+]
+}
