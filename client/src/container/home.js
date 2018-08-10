1f3c260b3e4b2b2534928c88f70c8b50d64b006b
Chay Dahill <cfdahill@gmail.com>
2018-08-09 17:56:48 -0700

    have youtube API functioning

---
+++ client/src/container/home.js
@@ -0,0 +1,39 @@
+import React, {Component} from "react";
+// import ReactDOM from "react-dom";
+import YouTube from 'simple-youtube-api';
+
+const API_KEY = "AIzaSyAa2utLyTh64jxHh57CbHykG8gsFdR3REU";
+const youtube = new YouTube(API_KEY);
+
+export default class Home extends Component {
+state= {
+    videos: []
+}
+
+componentWillMount = () => {
+youtube.getPlaylist('https://www.youtube.com/playlist?list=PLq_9nsuQGYzAC7T3b3Bzju7g18AP8dRrH')
+    .then(playlist => {
+        // console.log(playlist);
+        playlist.getVideos()
+            .then(videos => {
+                // console.log(videos);
+                this.setState({videos}, () => {console.log(this.state.videos)});
+            })
+            .catch(console.log);
+    })
+    .catch(console.log);
+}
+
+render() {
+    return(
+    <div>
+        {this.state.videos.map(video => {
+            return(<div key={video.id} id={video.id}>
+                <h1>{video.title}</h1>
+                <p>{video.description}</p>
+                <img src={video.thumbnails.medium.url} alt="video thumbnail" />
+            </div>);
+        })}
+    </div>
+)}
+}
